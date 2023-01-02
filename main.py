03639b5bb0a38d61d142e9ea928037dc02c717b3
Ivan Milinovic <ivan.milinovic123@gmail.com>
2023-01-02 22:12:52 +0100

    add logging, board cleaning, move simulation bugfix

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from typing import List, Tuple
+import logging
 
 import numpy as np
+
 
 from tetris_app import TetrisApp, rotate_clockwise, check_collision
 
@@ -31,19 +33,22 @@ class TetrisAgent():
         return sum
 
 
-    def get_parameters(self, state) -> List[int]:
+    def get_board_parameters(self, board) -> List[int]:
         return [
-            self.get_height_difference(state["board"]),
-            self.get_total_height(state["board"]),
-            self.get_number_of_holes(state["board"]),
+            self.get_height_difference(board),
+            self.get_total_height(board),
+            self.get_number_of_holes(board),
         ]
 
 
     def simulate_move(self, column: int, rotation_cnt: int) -> np.ndarray:
         stone = self.state['stone']
+        if check_collision(self.state['board'], stone, (column, 0)):
+            return None # unable to rotate stone
+
         for i in range(rotation_cnt):
             stone = rotate_clockwise(stone)
-            if check_collision(self.state['board'], stone, (0, column)):
+            if check_collision(self.state['board'], stone, (column, 0)):
                return None # unable to rotate stone
 
         stone = np.array(stone)
@@ -54,7 +59,7 @@ class TetrisAgent():
         height_offset = stone.shape[0]
         start_height = max(min_height - height_offset, 0)
 
-        while not check_collision(board, stone, (start_height, column)):
+        while not check_collision(board, stone, (column, start_height+1)):
             start_height += 1
 
         for i in range(stone.shape[0]):
@@ -65,9 +70,29 @@ class TetrisAgent():
         return board
 
 
+    def clean_board(self, board: np.ndarray) -> int:
+        """Modifies the board by removing full rows and returns number of cleared rows"""
+        non_zero_rows = ((board != 0).sum(1) != board.shape[1])
+        non_zero_rows[-1] = True
+        new_board = board[non_zero_rows]
+        
+        rows_cleared = board.shape[0] - new_board.shape[0]
+        empty_rows = np.zeros((rows_cleared, board.shape[1]), dtype=int)
+
+        if rows_cleared > 0:
+            board = np.concatenate((empty_rows, new_board), axis=0)
+        return rows_cleared
+
+
     def evaluate_move(self, column: int, rotation_cnt: int) -> float:
-        board = self.simulate_move(column, rotation_cnt)
-        # TO DO
+        if (board := self.simulate_move(column, rotation_cnt)) is None:
+            return 0
+
+        rows_cleared = self.clean_board(board)
+        if rows_cleared > 0:
+            parameters = self.get_board_parameters(board)
+            logging.info(rows_cleared, parameters)
+
         return 0
 
     def find_optimal_move(self) -> Tuple[int, int]:
@@ -79,7 +104,7 @@ class TetrisAgent():
                 if value < min_value:
                     min_value = value
                     move = (column, rotation_cnt)
-                return move
+
         return move
         
 
@@ -89,7 +114,6 @@ class TetrisAgent():
         while(1):
             #sleep(1)            
             self.state = self.tetrisApp.get_state()
-            print(self.state)
             optimal_move = self.find_optimal_move()
 
             """
@@ -113,6 +137,7 @@ class TetrisAgent():
             
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
     app = TetrisApp()
     agent = TetrisAgent(app)
     agent.start()
